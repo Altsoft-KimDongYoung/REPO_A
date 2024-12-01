42c93cea8f4bdf825d260d4b702949e8c44d1cc5
Altsoft-KimDongYoung <kim.dy@altsoft.kr>
2024-12-01 23:21:06 +0900

    ㅇㅇ

---
+++ dist/index.d.ts
@@ -0,0 +1,1 @@
+export type { User, Product } from "./types";
---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export type User = {
+  id: number;
+  name: string;
+};
+
+export type Product = {
+  id: number;
+  title: string;
+};
